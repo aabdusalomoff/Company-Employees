4c5947baa748c3ef9d7da7d4163cab2e28a3cf13
Azizbek <aziz.abdusalomov555@mail.ru>
2025-12-08 09:50:37 +0500

    saved

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 
